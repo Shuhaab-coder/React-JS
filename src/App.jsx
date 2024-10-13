c7955d29949938cb13a2c80a5a72dd62368be9fb
Shuhaab-coder <zarger546@gmail.com>
2024-10-13 11:29:47 +0530

    Edited <p></p> in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
             <div className="header">
                 <div className="logo">
                 <img src= "./src/react-logo.png" alt='logo' />
-                <p>React JS!</p>
+                <p>React JS Is The Best!</p>
                 </div>
                 <button onClick={() => document.write('Website Deleted. Press Refresh If You Want The Site To Come Back!')}>Click Me To Remove Everything in this website!</button>
         </div>
