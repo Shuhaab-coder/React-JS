2cb8e5bb1dd6539343aa088d72ce3c8bd150a548
Shuhaab-coder <zarger546@gmail.com>
2024-10-13 14:21:38 +0530

    Made the button be at the center and changed he logo and the p etc.

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,12 @@ const App = () => {
             <div className="header">
                 <div className="logo">
                 <img src= "./src/react-logo.png" alt='logo' />
-                <p>React JS Is The Best!</p>
+                <p>First Time Using React JS!</p>
                 </div>
                 <button onClick={() => document.write('Website Deleted. Press Refresh If You Want The Site To Come Back!')}>Click Me To Remove Everything in this website!</button>
         </div>
         </div>
-        </div>
+        </div>  
     );
 };  
                            
